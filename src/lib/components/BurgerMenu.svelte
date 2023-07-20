c99dd255911934c17c45f763854291bdab591032
Larisa Varga <larisa@entropyfox.com>
2023-07-20 17:38:55 +0200

    add some weird animation on page switch... there is a problem with my setup and I keep getting weird errors

--- src/lib/components/BurgerMenu.svelte
+++ src/lib/components/BurgerMenu.svelte
@@ -21,15 +21,15 @@
 
 <div class:open={isMenuOpen}>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div class="burger-menu fixed top-2.5 right-2.5 flex flex-col justify-between	w-7 h-6 cursor-pointer z-50" on:click={toggleMenu}>
+  <div class="burger-menu fixed top-2.5 right-2.5 flex md:hidden flex-col justify-between	w-7 h-6 cursor-pointer z-50" on:click={toggleMenu}>
     <div class="line w-full h-1 bg-black rounded-sm"></div>
     <div class="line w-full h-1 bg-black rounded-sm"></div>
     <div class="line w-full h-1 bg-black rounded-sm"></div>
   </div>
 
-  <div class="menu fixed top-0 right-0 w-72 h-full bg-white shadow-md z-40" transition:fly={{ x: isMenuOpen ? 0 : '100%', duration: 300 }}>
-    <ul class="p-0 m-4 pt-6 flex flex-col">
-			<li class="menu-item mb-4" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+  <div class="menu fixed top-0 right-0 w-72 md:w-full h-full md:h-12 bg-white shadow-md z-40" transition:fly={{ x: isMenuOpen ? 0 : '100%', duration: 300 }}>
+    <ul class="p-0 m-4 md:m-0 pt-6 md:pt-0 flex flex-col md:flex-row">
+			<li class="menu-item mb-4 md:mb-0" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
 			<li class="menu-item mb-4" aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
@@ -40,28 +40,31 @@
 </div>
 
   <style>
-    .burger-menu .line {
-      transition: transform 0.3s ease;
+    @media (max-width: 768px) {
+      .burger-menu .line {
+        transition: transform 0.3s ease;
+      }
+      .open .burger-menu .line {
+        transition: transform 0.3s ease;
+      }
+      .open .burger-menu .line:nth-child(1) {
+        transform: rotate(45deg) translate(7px, 7px);
+      }
+      .open .burger-menu .line:nth-child(2) {
+        opacity: 0;
+      }
+      .open .burger-menu .line:nth-child(3) {
+        transform: rotate(-45deg) translate(7px, -7px);
+      }
+      .menu {
+        transform: translate(100%, 0);
+        transition: transform 0.3s ease;
+      }
+      .open .menu { 
+        transform: translate(0%, 0);
+        transition: transform 0.3s ease;
+      }
     }
-    .open .burger-menu .line {
-      transition: transform 0.3s ease;
-    }
-    .open .burger-menu .line:nth-child(1) {
-      transform: rotate(45deg) translate(7px, 7px);
-    }
-    .open .burger-menu .line:nth-child(2) {
-      opacity: 0;
-    }
-    .open .burger-menu .line:nth-child(3) {
-      transform: rotate(-45deg) translate(7px, -7px);
-    }
-    .menu {
-      transform: translate(100%, 0);
-      transition: transform 0.3s ease;
-    }
-    .open .menu { 
-      transform: translate(0%, 0);
-      transition: transform 0.3s ease;
-    }
+
 
   </style>
